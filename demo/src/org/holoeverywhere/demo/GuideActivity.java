8cf34243761508d071d864999477dd9be4db5ef1
bfiveplus <bfiveplus@gmail.com>
2014-06-02 23:33:07 +0800

    create guide page and index page

---
+++ demo/src/org/holoeverywhere/demo/GuideActivity.java
@@ -0,0 +1,14 @@
+package org.holoeverywhere.demo;
+
+import org.holoeverywhere.app.Activity;
+
+import android.os.Bundle;
+
+public class GuideActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+	}
+}
--- demo/src/org/holoeverywhere/demo/fragments/index/IndexFragment.java
+++ demo/src/org/holoeverywhere/demo/fragments/index/IndexFragment.java
@@ -1,9 +1,14 @@
-package org.holoeverywhere.demo.fragments;
+package org.holoeverywhere.demo.fragments.index;
 
 import org.holoeverywhere.LayoutInflater;
 import org.holoeverywhere.app.Fragment;
 import org.holoeverywhere.demo.R;
+import org.holoeverywhere.demo.fragments.OtherFragment;
+import org.holoeverywhere.demo.fragments.about.AboutFragment;
+import org.holoeverywhere.demo.fragments.lists.ListsFastScrollWithSectionsFragment;
+import org.holoeverywhere.demo.fragments.pickers.PickersFragment;
 import org.holoeverywhere.demo.fragments.tabber.TabsTabsSwipeFragment;
+import org.holoeverywhere.widget.RadioButton;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
@@ -11,12 +16,10 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.RadioButton;
 
-public class PagesFragment extends Fragment implements OnClickListener {
+public class IndexFragment extends Fragment implements OnClickListener {
 
-    private RadioButton mRbNavBtn1;
-    private RadioButton mRbNavBtn2;
+    private RadioButton mRbNavBtn1, mRbNavBtn2,mRbNavBtn3,mRbNavBtn4,mRbNavBtn5;
 
     private FragmentManager fManager;
     private FragmentTransaction fTransaction;
@@ -31,9 +34,15 @@ public class PagesFragment extends Fragment implements OnClickListener {
         super.onActivityCreated(savedInstanceState);
         mRbNavBtn1 = (RadioButton) getActivity().findViewById(R.id.rb_nav_btn1);
         mRbNavBtn2 = (RadioButton) getActivity().findViewById(R.id.rb_nav_btn2);
+        mRbNavBtn3 = (RadioButton) getActivity().findViewById(R.id.rb_nav_btn3);
+        mRbNavBtn4 = (RadioButton) getActivity().findViewById(R.id.rb_nav_btn4);
+//        mRbNavBtn5 = (RadioButton) getActivity().findViewById(R.id.rb_nav_btn5);
         mRbNavBtn1.setChecked(true);
         mRbNavBtn1.setOnClickListener(this);
         mRbNavBtn2.setOnClickListener(this);
+        mRbNavBtn3.setOnClickListener(this);
+        mRbNavBtn4.setOnClickListener(this);
+//        mRbNavBtn5.setOnClickListener(this);
         fManager = getChildFragmentManager();
         
         fTransaction = fManager.beginTransaction();
@@ -52,6 +61,15 @@ public class PagesFragment extends Fragment implements OnClickListener {
         case R.id.rb_nav_btn2:
             fTransaction.replace(R.id.layout_container, new TabsTabsSwipeFragment());
             break;
+        case R.id.rb_nav_btn3:
+            fTransaction.replace(R.id.layout_container, new PickersFragment());
+            break;
+        case R.id.rb_nav_btn4:
+            fTransaction.replace(R.id.layout_container, new ListsFastScrollWithSectionsFragment());
+            break;
+//        case R.id.rb_nav_btn5:
+//            fTransaction.replace(R.id.layout_container, new AboutFragment());
+//            break;
         }
         fTransaction.commit();
     }
